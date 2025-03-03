4508092b9608b723f9b8d7f38de359a68efabc23
OleSton <65806680+OleSton@users.noreply.github.com>
2025-03-03 18:10:35 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,12 @@
-/*
-'use strict';
 
-importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html","styles.css"]);
-
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-
-toolbox.router.get('/*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
-});
-*/
 // named cache in Cache Storage
 const CACHE_NAME = 'devtools-tips-v3';
 
 // list of requests whose responses will be pre-cached at install
 const INITIAL_CACHED_RESOURCES = [
-    '/',
-    '/offline/',
-    '/all/',
-    '/browser/edge/',
-    '/browser/safari/',
-    '/browser/firefox/',
-    '/browser/chrome/',
+    
     '/pwa/styles.css',
-    '/pwa/index.html',
+    '/pwa/index.html'
     
 ];
 
