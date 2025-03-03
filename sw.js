9c3d1135db1b11eb53687801a150abfe60a06ff7
OleSton <65806680+OleSton@users.noreply.github.com>
2025-03-03 20:06:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,19 @@
 
 // named cache in Cache Storage
 const CACHE_NAME = 'devtools-tips-v3';
-
-// list of requests whose responses will be pre-cached at install
-const INITIAL_CACHED_RESOURCES = [
-    
-    '/pwa/styles.css',
-    '/pwa/index.html'
-    
-];
-
-// install event handler (note async operation)
-// opens named cache, pre-caches identified resources above
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        (async () => {
-            const cache = await caches.open(CACHE_NAME);
-            cache.addAll(INITIAL_CACHED_RESOURCES);
-        })(),
-    );
+const STATIC_CACHE_URLS = ["/", "/pwa/styles.css", "/pwa/index.html"];
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_CACHE_URLS))
+  );
 });
 
-
-self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-
-  // Don't care about other-origin URLs.
-  if (url.origin !== location.origin) {
-    return;
-  }
-
-  // Don't care about anything else than GET.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
-  // Don't care about widget requests.
-  if (url.pathname.includes("/widgets/")) {
-    return;
-  }
-
- 
+self.addEventListener("fetch", (event) => {
+  // Cache-First Strategy
+  event.respondWith(
+    caches
+      .match(event.request) // check if the request has already been cached
+      .then((cached) => cached || fetch(event.request)) // otherwise request network
+  );
 });
